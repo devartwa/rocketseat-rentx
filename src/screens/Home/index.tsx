8321abd890220475d570eb12875065d0b3cca58a
Arthur Bueno <arthurbueno@MacBook-Air-de-Arthur.local>
2021-12-07 08:50:34 -0300

    added: header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native';
+import {
+  Container,
+  HeaderWrapper,
+  Header,
+  TotalCars,
+} from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo from '../../assets/logo.svg';
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <HeaderWrapper>
+        <Header>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+        </Header>
+        <TotalCars>
+          Total de 12 carros
+        </TotalCars>
+      </HeaderWrapper>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,28 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
+export const Header = styled.View`
+  margin-top: ${RFValue(30)}px;
+`;
+
+export const HeaderWrapper = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-horizontal: 20px;
+`;
+
+export const TotalCars = styled.Text`
+  margin-top: ${RFValue(30)}px;
+  font-size: ${RFValue(15)}px;
   font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
