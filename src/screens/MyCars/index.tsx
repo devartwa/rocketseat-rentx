ef4e4fe54540d5ed1887cdbee964c96631a3bcf7
Arthur Bueno <arthurbuenoneto@gmail.com>
2021-12-19 13:36:38 -0300

    added: my cars completed

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,18 +1,56 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { CarListModel } from '../../@types';
+import { CarListModel, MainParamList } from '../../@types';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { BackButton } from '../../components/BackButton';
+import { AntDesign } from '@expo/vector-icons';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { Car } from '../../components/Car';
+import { Load } from '../../components/Load';
 import queryString from 'query-string';
 import requester from '../../services/requester';
 import services from '../../services/services';
 
 import {
-  Container
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarList,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterDate,
+  CarFooterPeriod,
+  EmptyContainer,
+  EmptyText,
 } from './styles';
 
-export function MyCars() {
-  const [cars, setCars] = useState<CarListModel[]>([]);
+type MyCarsNavigationProp = StackNavigationProp<MainParamList, 'MyCars'>;
+type MyCarsProps = { navigation: MyCarsNavigationProp };
+
+interface CarProps {
+  car: CarListModel;
+  user_id: number;
+  id: number;
+  startDate: string;
+  endDate: string;
+}
+
+export function MyCars({ navigation }: MyCarsProps) {
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
 
   const isMounted = useRef(true);
+  const theme = useTheme();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  }
 
   async function getCars() {
     const user_id = 1;
@@ -44,7 +82,71 @@ export function MyCars() {
 
   return (
     <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton
+          onPress={handleGoBack}
+          color={theme.colors.shape}
+        />
 
+        <Title>
+          Seus agendamentos,{'\n'}
+          estão aqui.
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+      {loading ? (
+        <Load />
+      ) : cars === null || cars.length <= 0 ? (
+        <EmptyContainer>
+          <AntDesign
+            name="exclamationcircleo"
+            size={32}
+            color={theme.colors.main}
+            style={{ marginBottom: 25 }}
+          />
+          <EmptyText>Você não possui nenhum carro alugado.</EmptyText>
+        </EmptyContainer>
+      ) : (
+        (
+          <Content>
+            <Appointments>
+              <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+              <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+            </Appointments>
+            <CarList
+              data={cars}
+              keyExtractor={item => String(item.id)}
+              renderItem={({ item }) => (
+                <CarWrapper>
+                  <Car data={item.car} />
+                  <CarFooter>
+                    <CarFooterTitle>Período</CarFooterTitle>
+                    <CarFooterPeriod>
+                      <CarFooterDate>{item.startDate}</CarFooterDate>
+                      <AntDesign
+                        name="arrowright"
+                        size={20}
+                        color={theme.colors.title}
+                        style={{ marginHorizontal: 10 }}
+                      />
+                      <CarFooterDate>{item.endDate}</CarFooterDate>
+                    </CarFooterPeriod>
+                  </CarFooter>
+                </CarWrapper>
+              )}
+            />
+          </Content>
+        )
+      )
+      }
     </Container>
   );
 };
